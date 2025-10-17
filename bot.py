caabdd01fe80bb08b9037b1e65b50f812df187f0
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-17 14:18:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,9 @@ from discord.ui import Modal, TextInput
 from datetime import datetime
 import asyncpg 
 from discord.ui import View, Button
-from discord.ui import Select, View, SelectOption
-from discord import SelectOption, Embed
+from discord.ui import Select, View
+from discord.ui import View, Select, SelectOption
+
 import aiohttp
 import io
 
