830327c67553f41fbf3d912e23eb7aef237b9571
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-18 22:21:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1476,7 +1476,7 @@ class DatabaseView(View):
                 per_page=5,
                 author_id=interaction.user.id
             )
-            embeds = view.__build_embeds_for_current_page()
+            embeds = view._build_embeds_for_current_page()
             await interaction.response.edit_message(content=None, embeds=embeds, view=view)
 
 
