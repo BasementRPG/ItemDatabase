ab32af362e057ea453f98a714d407d5a05280865
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-20 14:39:29 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,14 @@ set -e
 
 # 🧱 Install Chromium dependencies
 apt-get update && apt-get install -y \
-    libglib2.0-0 libgobject-2.0-0 libnss3 libnspr4 \
-    libnssutil3 libsmime3 libcups2 libatk1.0-0 \
-    libatk-bridge2.0-0 libxcomposite1 libxdamage1 \
-    libxrandr2 libgbm1 libpango-1.0-0 libasound2 \
-    libxshmfence1 libxkbcommon0 libx11-xcb1 libdrm2 \
-    libdbus-1-3 libxcb1 libxext6 libxfixes3 \
-    fonts-liberation libappindicator3-1 lsb-release wget xdg-utils
+  libglib2.0-0 libnss3 libnspr4 libdbus-1-3 \
+  libatk1.0-0 libatk-bridge2.0-0 libcups2 \
+  libxcomposite1 libxdamage1 libxrandr2 libgbm1 \
+  libpango-1.0-0 libasound2 libxshmfence1 \
+  libxkbcommon0 libx11-xcb1 libdrm2 \
+  libxcb1 libxext6 libxfixes3 \
+  fonts-liberation libappindicator3-1 \
+  lsb-release wget xdg-utils ca-certificates curl
 
 # 🧩 Install Playwright browser (Chromium)
 npx playwright install --with-deps chromium
