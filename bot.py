d1628884a1038c7d69fd1eca06bbed59afa61e71
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-22 21:00:58 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2434,9 +2434,11 @@ async def view_wiki_items(interaction: discord.Interaction, slot: app_commands.C
                 draw.text((x, y), stat, font=font_stats, fill="white")
                 y += font.size + line_spacing  # add font height + spacing
         
-           """# Stats block (wrapped for readability)
+            """
+            # Stats block (wrapped for readability)
             wrapped_stats = "\n".join(stats.splitlines()[:8])
-            draw.text((110, 55), wrapped_stats, font=font_stats, fill=text_color)"""
+            draw.text((110, 55), wrapped_stats, font=font_stats, fill=text_color)
+            """
             
             # Save to buffer
             buffer = io.BytesIO()
