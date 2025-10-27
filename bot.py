70e08aff665a3ee9924169728a8cf4e0ccbdaa83
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-27 19:12:39 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -371,7 +371,7 @@ class ItemDatabaseModal(discord.ui.Modal, title="Add Item to Database"):
 
     
             # Confirmation
-            await interaction.edit_original_response(
+            await interaction.response.edit_message(
                 f"✅ `{item_name}` added successfully!",
                 ephemeral=True
             )
