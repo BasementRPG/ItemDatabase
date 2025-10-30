ffc47dd39481c3d9a803f13433f7431cb279df67
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-30 11:28:33 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2020,7 +2020,7 @@ async def run_wiki_items(interaction: discord.Interaction, slot: str, stat: Opti
                             item_name, item_slot, item_image, npc_image, npc_name, zone_name, zone_area,
                             item_stats, description, crafted_name, crafting_recipe, quest_name, npc_level,
                             added_by, source
-                        ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,'Wiki')
+                        ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,'Wiki')
                         ON CONFLICT (item_name) DO NOTHING
                     """,
                     item["item_name"],
